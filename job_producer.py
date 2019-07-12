1c920ce1aafa6f118b1f6874dafa379712e87430
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-12 14:47:31 +0100

    Create topic automatically in producer

--- job_producer.py
+++ job_producer.py
@@ -1,6 +1,19 @@
 import argparse
 import sys
 from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+
+
+def create_job_queue_topic(topic_name: str, number_of_partitions: int, broker: str):
+    admin_client = AdminClient({'bootstrap.servers': broker})
+    fs = admin_client.create_topics([NewTopic(topic_name, num_partitions=3, replication_factor=number_of_partitions)])
+
+    for topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            print("Topic {} created".format(topic))
+        except Exception as e:
+            print("Failed to create topic {}: {}".format(topic, e))
 
 
 if __name__ == "__main__":
@@ -8,8 +21,10 @@ if __name__ == "__main__":
     parser.add_argument("--broker", help="Address for the Kafka broker", default="localhost")
     parser.add_argument("--topic", help="Topic to publish messages to", default="job_queue")
     parser.add_argument("--number-of-jobs", default=100)
+    parser.add_argument("--consumers-in-pool", default=10)
     args = parser.parse_args()
-    print(args.broker)
+
+    create_job_queue_topic(args.topic, args.consumers_in_pool, args.broker)
 
     conf = {'bootstrap.servers': args.broker}
     p = Producer(**conf)
